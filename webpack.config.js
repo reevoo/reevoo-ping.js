24df07e058605264a919c997698ffdc520f00304
Jonny Arnold <jonny.arnold@reevoo.com>
2015-12-07 16:51:34 +0000

    Fix spacing issues.

--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,38 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    // Webpack entry point.
-    entry: './lib/reevoo-ping.js',
+  // Webpack entry point.
+  entry: './lib/reevoo-ping.js',
 
-    // Output definition.
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'reevoo-ping.js'
-    },
+  // Output definition.
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'reevoo-ping.js'
+  },
 
-    // Define loaders for webpack transpilation.
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                test: path.join(__dirname, 'lib'),
-                query: {
-                  presets: 'es2015',
-                },
-            }
-        ]
-    },
+  // Define loaders for webpack transpilation.
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: path.join(__dirname, 'lib'),
+        query: {
+          presets: 'es2015',
+        },
+      }
+    ]
+  },
 
-    plugins: [
-        // Avoid publishing files when compilation fails
-        new webpack.NoErrorsPlugin()
-    ],
+  plugins: [
+    // Avoid publishing files when compilation fails
+    new webpack.NoErrorsPlugin()
+  ],
 
-    stats: {
-        // Nice colored output
-        colors: true
-    },
-    
-    // Create Sourcemaps for the bundle
-    devtool: 'source-map',
+  stats: {
+    // Nice colored output
+    colors: true
+  },
+
+  // Create Sourcemaps for the bundle
+  devtool: 'source-map',
 };
