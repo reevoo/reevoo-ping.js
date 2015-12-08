b7734bbc71c7541bfe570bdc21493a92b4be4217
Jonny Arnold <jonny.arnold@reevoo.com>
2015-12-08 15:54:47 +0000

    Added debugging helper.

--- config/test.js
+++ config/test.js
@@ -1,3 +1,4 @@
 module.exports = {
   COLLECTOR_URI: 'https://COLLECTOR-URL',
+  DEBUG: false,
 };
---
+++ spec/debug.spec.js
@@ -0,0 +1,7 @@
+import debug from 'lib/debug';
+
+describe('lib/debug', () => {
+  it('is simple enough not to have tests', () => {
+    expect(debug).toBeDefined();
+  });
+});
