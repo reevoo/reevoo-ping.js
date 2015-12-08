fc27fbac15d2f12bddf24b0aa49175241f10e22e
Jonny Arnold <jonny.arnold@reevoo.com>
2015-12-08 15:54:13 +0000

    Snowplow module is now looking bang tidy.

--- spec/snowplow.spec.js
+++ spec/snowplow.spec.js
@@ -2,18 +2,14 @@ import snowplow from 'lib/snowplow';
 
 describe('lib/snowplow', () => {
   describe('after it is required', () => {
-    it('defines the GlobalSnowplowNamespace', () => {
-      expect(window.GlobalSnowplowNamespace).toBeDefined();
+    it('adds a newTracker event to the snowplow queue', () => {
+      pending('I don\'t know how to inspect the Snowplow Q!');
     });
   });
 
   describe('export', () => {
     it('exports a function', () => {
       expect(snowplow).toBeDefined();
-    });
-
-    it('exports an asynchronous Snowplow function', () => {
-      expect(snowplow.q).toBeDefined();
     });
   });
 });
---
+++ spec/snowplow/bootstrap.spec.js
@@ -0,0 +1,19 @@
+import snowplow from 'lib/snowplow/bootstrap';
+
+describe('lib/snowplow/bootstrap', () => {
+  describe('after it is required', () => {
+    it('defines the GlobalSnowplowNamespace', () => {
+      expect(window.GlobalSnowplowNamespace).toBeDefined();
+    });
+  });
+
+  describe('export', () => {
+    it('exports a function', () => {
+      expect(snowplow).toBeDefined();
+    });
+
+    it('exports an asynchronous Snowplow function', () => {
+      expect(snowplow.q).toBeDefined();
+    });
+  });
+});
