423f969ccf3cc3d732b798c55ff455594205a6d8
Jonny Arnold <jonny.arnold@reevoo.com>
2015-12-10 17:08:39 +0000

    WIP Add tests for reevoo-ping.js; move to use validate.js

--- spec/reevoo-ping.spec.js
+++ spec/reevoo-ping.spec.js
@@ -1,7 +1,86 @@
-import 'lib/reevoo-ping';
+import reevooPingInjector from 'inject!lib/reevoo-ping';
 
 describe('lib/reevoo-ping', () => {
-  it('defines reevooPing', () => {
-    expect(window.reevooPing).toBeDefined();
+  let ReevooPing;
+  let snowplow;
+
+  beforeEach(() => {
+    snowplow = jasmine.createSpy('snowplow');
+    ReevooPing = reevooPingInjector({
+      './snowplow': snowplow,
+    }).default;
+  });
+
+  it('defines ReevooPing', () => {
+    expect(ReevooPing).toBeDefined();
+  });
+
+  describe('.constructor', () => {
+    it('throws an error if the trkref is not given', () => {
+      expect(() => {
+        new ReevooPing({}); // eslint-disable-line no-new
+      }).toThrowError(/Trkref/);
+    });
+
+    it('returns object with valid options', () => {
+      expect(() => {
+        new ReevooPing({ trkref: 'TRKREF' }); // eslint-disable-line no-new
+      }).not.toThrowError();
+    });
+  });
+
+  let ping;
+  beforeEach(() => {
+    ping = new ReevooPing({ trkref: 'TRKREF' });
+  });
+
+  describe('.pageViewed', () => {
+    it('calls Snowplow', () => {
+      ping.pageViewed();
+      expect(snowplow).toHaveBeenCalledWith('trackPageView');
+    });
+  });
+
+  describe('.heartbeat', () => {
+    it('calls Snowplow', () => {
+      ping.heartbeat();
+      expect(snowplow).toHaveBeenCalledWith(
+        'enableActivityTracking',
+        jasmine.anything(),
+        jasmine.anything()
+      );
+    });
+
+    it('raises an error if called after .pageViewed', () => {
+      ping.pageViewed();
+
+      expect(() => {
+        ping.heartbeat();
+      }).toThrowError(/pageViewed/);
+    });
+  });
+
+  describe('.badgeRendered', () => {
+    const badgeParams = {
+      hit_type: 'hit',
+      sku: 'SKU',
+      average_score: 9.2,
+      badge_type: 'product_reviews',
+      badge_variant: 'default',
+      badge_name: 'terry',
+    };
+
+    it('calls Snowplow', () => {
+      ping.badgeRendered(badgeParams);
+      expect(snowplow).toHaveBeenCalled();
+    });
+
+    it('raises an error if hit type is not supplied', () => {
+      badgeParams.hit_type = undefined;
+
+      expect(() => {
+        ping.badgeRendered(badgeParams);
+      }).toThrowError(/hit_type/);
+    });
   });
 });
