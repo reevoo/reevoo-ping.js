4bd6b9c71a80d4b2da5d9e75d20ee3c55f70fa5b
David Sevcik <david.sevcik@gmail.com>
2016-04-11 18:17:34 +0200

    Update badge.rendered tracking for new snowplow schema (#17)
    * Update badge.rendered tracking for new snowplow schema
    https://github.com/reevoo/reevoo-event-dictionary/blob/master/schemas/com.reevoo/badge_rendered/jsonschema/2-0-0
    
    * Test coverage improved

--- spec/events/badge.spec.js
+++ spec/events/badge.spec.js
@@ -1,12 +1,13 @@
 import badgeInjector from 'inject!lib/events/badge';
 
 describe('lib/events/badge', () => {
+  let Badge;
   let badge;
   let snowplow;
 
   beforeEach(() => {
     snowplow = jasmine.createSpy('snowplow');
-    const Badge = badgeInjector({
+    Badge = badgeInjector({
       '../snowplow': snowplow,
     }).default;
 
@@ -19,23 +20,28 @@ describe('lib/events/badge', () => {
 
     beforeEach(() => {
       badgeRenderedParams = {
-        hit_type: 'hit',
+        hitType: 'impression',
         trkref: 'TRKREF',
         sku: 'SKU',
-        average_score: 9.2,
-        badge_type: 'product_reviews',
-        badge_variant: 'default',
-        badge_name: 'terry',
+        contentType: 'reviewable',
+        ctaPageUse: 'single_reviewable',
+        ctaStyle: 'terry',
+        averageScore: 9.2,
       };
     });
 
     it('calls Snowplow', () => {
       badge.rendered(badgeRenderedParams);
-      expect(snowplow).toHaveBeenCalled();
+      expect(snowplow).toHaveBeenCalledWith(
+        jasmine.anything(),
+        jasmine.objectContaining({
+          data: badgeRenderedParams,
+        }),
+      );
     });
 
     it('raises an error if hit type is not supplied', () => {
-      badgeRenderedParams.hit_type = undefined;
+      badgeRenderedParams.hitType = undefined;
 
       expect(() => {
         badge.rendered(badgeRenderedParams);
@@ -43,19 +49,27 @@ describe('lib/events/badge', () => {
     });
 
     it('raises an error if hit type is not valid', () => {
-      badgeRenderedParams.hit_type = 'NOT_A_REAL_HIT_TYPE';
+      badgeRenderedParams.hitType = 'NOT_A_REAL_HIT_TYPE';
 
       expect(() => {
         badge.rendered(badgeRenderedParams);
-      }).toThrowError();
+      }).toThrowError(/not included in the list/);
     });
 
-    it('raises an error if badge type is not supplied', () => {
-      badgeRenderedParams.badge_type = undefined;
+    it('raises an error if content type is not supplied', () => {
+      badgeRenderedParams.contentType = undefined;
 
       expect(() => {
         badge.rendered(badgeRenderedParams);
-      }).toThrowError(/Badge type/);
+      }).toThrowError(/Content type/);
+    });
+
+    it('raises an error if content type is not valid', () => {
+      badgeRenderedParams.contentType = 'NOT_A_REAL_CONTENT_TYPE';
+
+      expect(() => {
+        badge.rendered(badgeRenderedParams);
+      }).toThrowError(/not included in the list/);
     });
 
     it('raises an error if trkref is not supplied', () => {
@@ -66,20 +80,109 @@ describe('lib/events/badge', () => {
       }).toThrowError(/Trkref/);
     });
 
-    it('includes a JSON-encoded string of the options given', () => {
-      const expectedJsonString = JSON.stringify(badgeRenderedParams);
+    it('uses TRKREF from the root scope when not provided as argument', () => {
+      badgeRenderedParams.trkref = undefined;
+      badge = new Badge('MY_TRKREF');
 
       badge.rendered(badgeRenderedParams);
-
-      // payload.data.fullBadgeParams contains our JSON string.
       expect(snowplow).toHaveBeenCalledWith(
         jasmine.anything(),
         jasmine.objectContaining({
           data: jasmine.objectContaining({
-            fullBadgeParams: expectedJsonString,
+            trkref: 'MY_TRKREF',
           }),
         })
       );
+    });
+
+    it('raises an error if ctaPageUse is not valid', () => {
+      badgeRenderedParams.ctaPageUse = 'NOT_A_REAL_PAGE_USE';
+
+      expect(() => {
+        badge.rendered(badgeRenderedParams);
+      }).toThrowError(/not included in the list/);
+    });
+
+    it('sorts and stringifies reviewable context', () => {
+      badge.rendered({ ...badgeRenderedParams, reviewableContext: { model: 'Focus', manufacturer: 'Ford' }});
+
+      expect(snowplow).toHaveBeenCalledWith(
+        jasmine.anything(),
+        jasmine.objectContaining({
+          data: jasmine.objectContaining({
+            reviewableContext: '{"manufacturer":"Ford","model":"Focus"}',
+          }),
+        })
+      );
+    });
+
+    describe('determines CTA page use', () => {
+      it('uses multi_reviewable if badgeVariant contains listing', () => {
+        badge.rendered({ ...badgeRenderedParams, ctaPageUse: undefined, badgeVariant: 'my_listing123' });
+
+        expect(snowplow).toHaveBeenCalledWith(
+          jasmine.anything(),
+          jasmine.objectContaining({
+            data: jasmine.objectContaining({
+              ctaPageUse: 'multi_reviewable',
+            }),
+          })
+        );
+      });
+
+      it('uses multi_reviewable if badgeVariant contains category', () => {
+        badge.rendered({ ...badgeRenderedParams, ctaPageUse: undefined, badgeVariant: 'my_category123' });
+
+        expect(snowplow).toHaveBeenCalledWith(
+          jasmine.anything(),
+          jasmine.objectContaining({
+            data: jasmine.objectContaining({
+              ctaPageUse: 'multi_reviewable',
+            }),
+          })
+        );
+      });
+
+      it('uses single_reviewable otherwise', () => {
+        badge.rendered({ ...badgeRenderedParams, ctaPageUse: undefined, badgeVariant: 'my_foo123' });
+
+        expect(snowplow).toHaveBeenCalledWith(
+          jasmine.anything(),
+          jasmine.objectContaining({
+            data: jasmine.objectContaining({
+              ctaPageUse: 'single_reviewable',
+            }),
+          })
+        );
+      });
+    });
+
+    describe('determines CTA style', () => {
+      it('uses badgeName if available', () => {
+        badge.rendered({ ...badgeRenderedParams, ctaStyle: undefined, badgeName: 'my_badge', badgeVariant: 'default' });
+
+        expect(snowplow).toHaveBeenCalledWith(
+          jasmine.anything(),
+          jasmine.objectContaining({
+            data: jasmine.objectContaining({
+              ctaStyle: 'my_badge',
+            }),
+          })
+        );
+      });
+
+      it('uses badgeVariant if badgeName not available', () => {
+        badge.rendered({ ...badgeRenderedParams, ctaStyle: undefined, badgeName: undefined, badgeVariant: 'default' });
+
+        expect(snowplow).toHaveBeenCalledWith(
+          jasmine.anything(),
+          jasmine.objectContaining({
+            data: jasmine.objectContaining({
+              ctaStyle: 'default',
+            }),
+          })
+        );
+      });
     });
   });
 
