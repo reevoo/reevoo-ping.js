b51dad214262a4b3b6f699766fd289c5be7c0636
Victor Vila Ases <victor.vila@reevoo.com>
2016-08-11 16:03:53 +0100

    Changed libraryTarget to umd so the library can be used with different loaders. Bumped version to 2.5.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
 const REEVOO_ENV = process.env.REEVOO_ENV ? process.env.REEVOO_ENV : 'dev';
 
@@ -12,15 +12,16 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: 'reevoo-ping.js',
     library: 'ReevooPing',
-    libraryTarget: 'commonjs2',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
   },
 
   // Define loaders for webpack transpilation.
   module: {
     loaders: [
       {test: path.join(__dirname, 'lib'), loader: 'babel-loader', query: { presets: ['es2015', 'stage-1'] }},
-      {test: path.join(__dirname, 'lib'), loader: "eslint-loader"}
-    ]
+      {test: path.join(__dirname, 'lib'), loader: 'eslint-loader'},
+    ],
   },
 
   plugins: [
@@ -30,7 +31,7 @@ module.exports = {
     // Load config or the building environment
     new webpack.DefinePlugin({
       CONFIG: JSON.stringify(require(path.join(__dirname, 'config', REEVOO_ENV))),
-    })
+    }),
   ],
 
   // Only output warnings and errors.
@@ -40,7 +41,7 @@ module.exports = {
 
   stats: {
     // Nice colored output
-    colors: true
+    colors: true,
   },
 
   // Create Sourcemaps for the bundle
